d965e0afb12a0432d09d5c4505b0feaf479e8f62
MRegterschot <marijn.regterschot@gmail.com>
2025-02-17 21:18:45 +0100

    Add SendHideManialinkPageToId method to GbxClient for hiding manialink pages for specific clients

---
+++ TMSendHideManialinkPageToId.go
@@ -0,0 +1,7 @@
+package main
+
+// Hide the displayed manialink page on the client with the specified UId. Only available to Admin.
+func (client *GbxClient) SendHideManialinkPageToId(uid int) error {
+	_, err := client.Send("SendHideManialinkPageToId", uid)
+	return err
+}
