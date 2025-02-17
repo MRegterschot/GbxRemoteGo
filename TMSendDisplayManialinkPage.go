62429866bab12c26f8127e04f555accc7f223830
MRegterschot <marijn.regterschot@gmail.com>
2025-02-17 21:16:09 +0100

    Refactor SendDisplayManialinkPage methods in GbxClient to use Send for improved error handling

--- TMSendDisplayManialinkPage.go
+++ TMSendDisplayManialinkPage.go
@@ -1,18 +1,7 @@
 package main
 
-import "errors"
-
 // Display a manialink page on all clients. The parameters are the xml description of the page to display, a timeout to autohide it (0 = permanent), and a boolean to indicate whether the page must be hidden as soon as the user clicks on a page option. Only available to Admin.
-func (client *GbxClient) SendDisplayManialinkPage(xml CData, timeout int, hideOnClick bool) (bool, error) {
-	res, err := client.Call("SendDisplayManialinkPage", xml, timeout, hideOnClick)
-	if err != nil {
-		return false, err
-	}
-
-	successful, ok := res.(bool)
-	if !ok {
-		return false, errors.New("unexpected response format")
-	}
-
-	return successful, nil
+func (client *GbxClient) SendDisplayManialinkPage(xml CData, timeout int, hideOnClick bool) error {
+	_, err := client.Send("SendDisplayManialinkPage", xml, timeout, hideOnClick)
+	return err
 }
