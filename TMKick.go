c918aaaf5600647b652ae6e67bd8ba41ffd29e0f
MRegterschot <marijn.regterschot@gmail.com>
2025-02-17 21:27:54 +0100

    Add Kick method to GbxClient for kicking players with an optional reason

---
+++ TMKick.go
@@ -0,0 +1,7 @@
+package main
+
+// Kick the player with the specified login, with an optional message. Only available to Admin.
+func (client *GbxClient) Kick(login string, reason string) error {
+	_, err := client.Send("Kick", login, reason)
+	return err
+}
