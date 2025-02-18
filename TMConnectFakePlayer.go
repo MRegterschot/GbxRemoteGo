114cd855e9d5e2e88518bf7667ce40387d742f09
MRegterschot <marijn.regterschot@gmail.com>
2025-02-18 19:28:36 +0100

    Add fake player management methods to GbxClient: implement ConnectFakePlayer and DisconnectFakePlayer functions, remove deprecated TMConnectFakePlayer.go file

--- TMConnectFakePlayer.go
+++
@@ -1,7 +0,0 @@
-package main
-
-// (debug tool) Connect a fake player to the server. Only available to Admin.
-func (client *GbxClient) ConnectFakePlayer() error {
-	_, err := client.Send("ConnectFakePlayer")
-	return err
-}
--- TMFakePlayer.go
+++ TMFakePlayer.go
@@ -1,5 +1,11 @@
 package main
 
+// (debug tool) Connect a fake player to the server. Only available to Admin.
+func (client *GbxClient) ConnectFakePlayer() error {
+	_, err := client.Send("ConnectFakePlayer")
+	return err
+}
+
 // (debug tool) Disconnect a fake player, or all the fake players if login is '*'. Only available to Admin.
 func (client *GbxClient) DisconnectFakePlayer(login string) error {
 	_, err := client.Send("DisconnectFakePlayer", login)
