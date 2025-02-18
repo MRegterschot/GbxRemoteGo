2cf2a440096f23f197f2d2a73b7447412d3d9acb
MRegterschot <marijn.regterschot@gmail.com>
2025-02-18 23:08:34 +0100

    Add methods for opponent visibility and script management in GbxClient: implement SetForceShowAllOpponents, GetForceShowAllOpponents, SetScriptName, and GetScriptName functions; introduce TMForceShowAllOpponents and TMScriptName structs

--- methods.gameflow.go
+++ methods.gameflow.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/MRegterschot/GbxRemoteGo/structs"
+
 // Restarts the map with an optional boolean parameter DontClearCupScores
 func (client *GbxClient) RestartMap(dontClearCupScores ...bool) error {
 	var param interface{} = nil
@@ -24,4 +26,50 @@ func (client *GbxClient) NextMap(dontClearCupScores ...bool) error {
 func (client *GbxClient) AutoTeamBalance() error {
 	_, err := client.Call("AutoTeamBalance")
 	return err
+}
+
+// Set whether to override the players preferences and always display all opponents (0=no override, 1=show all, other value=minimum number of opponents). Only available to Admin. Requires a map restart to be taken into account.
+func (client *GbxClient) SetForceShowAllOpponents(opponents int) error {
+	_, err := client.Call("SetForceShowAllOpponents", opponents)
+	return err
+}
+
+// Get whether players are forced to show all opponents. The struct returned contains two fields CurrentValue and NextValue.
+func (client *GbxClient) GetForceShowAllOpponents() (structs.TMForceShowAllOpponents, error) {
+	res, err := client.Call("GetForceShowAllOpponents")
+	if err != nil {
+		return structs.TMForceShowAllOpponents{}, err
+	}
+
+	var opponents structs.TMForceShowAllOpponents
+	err = convertToStruct(res, &opponents)
+
+	if err != nil {
+		return structs.TMForceShowAllOpponents{}, err
+	}
+
+	return opponents, nil
+}
+
+// Set a new mode script name for script mode. Only available to Admin. Requires a map restart to be taken into account.
+func (client *GbxClient) SetScriptName(scriptName string) error {
+	_, err := client.Call("SetScriptName", structs.GetScriptByName(scriptName))
+	return err
+}
+
+// Get the current and next mode script name for script mode. The struct returned contains two fields CurrentValue and NextValue.
+func (client *GbxClient) GetScriptName() (structs.TMScriptName, error) {
+	res, err := client.Call("GetScriptName")
+	if err != nil {
+		return structs.TMScriptName{}, err
+	}
+
+	var scriptName structs.TMScriptName
+	err = convertToStruct(res, &scriptName)
+
+	if err != nil {
+		return structs.TMScriptName{}, err
+	}
+
+	return scriptName, nil
 }
---
+++ structs/gameflow.go
@@ -0,0 +1,56 @@
+package structs
+
+import "strings"
+
+type TMForceShowAllOpponents struct {
+	CurrentValue int `json:"CurrentValue" xmlrpc:"CurrentValue"`
+	NextValue    int `json:"NextValue" xmlrpc:"NextValue"`
+}
+
+type TMScriptName struct {
+	CurrentValue string `json:"CurrentValue" xmlrpc:"CurrentValue"`
+	NextValue    string `json:"NextValue" xmlrpc:"NextValue"`
+}
+
+// TMScriptMode represents a type for TrackMania Script Modes.
+type TMScriptMode string
+
+// Define all possible script modes.
+const (
+	ScriptTimeAttack TMScriptMode = "Trackmania/TM_TimeAttack_Online.Script.txt"
+	ScriptLaps       TMScriptMode = "Trackmania/TM_Laps_Online.Script.txt"
+	ScriptRounds     TMScriptMode = "Trackmania/TM_Rounds_Online.Script.txt"
+	ScriptCup        TMScriptMode = "Trackmania/TM_Cup_Online.Script.txt"
+	ScriptTeams      TMScriptMode = "Trackmania/TM_Teams_Online.Script.txt"
+	ScriptKnockout   TMScriptMode = "Trackmania/TM_Knockout_Online.Script.txt"
+	ScriptChampion   TMScriptMode = "Trackmania/Deprecated/TM_Champion_Online.Script.txt"
+	ScriptRoyal      TMScriptMode = "Trackmania/TM_RoyalTimeAttack_Online.Script.txt"
+	ScriptStunt      TMScriptMode = "Trackmania/TM_StuntMulti_Online.Script.txt"
+	ScriptPlatform   TMScriptMode = "Trackmania/TM_Platform_Online.Script.txt"
+)
+
+// Map of user input to script filenames
+var scriptMap = map[string]TMScriptMode{
+	"ta":        ScriptTimeAttack,
+	"timeattack": ScriptTimeAttack,
+	"laps":      ScriptLaps,
+	"rounds":    ScriptRounds,
+	"cup":       ScriptCup,
+	"team":      ScriptTeams,
+	"teams":     ScriptTeams,
+	"ko":        ScriptKnockout,
+	"knockout":  ScriptKnockout,
+	"champion":  ScriptChampion,
+	"royal":     ScriptRoyal,
+	"stunt":     ScriptStunt,
+	"platform":  ScriptPlatform,
+}
+
+// Convert user input into a valid script mode (or return empty if not found)
+func GetScriptByName(userInput string) string {
+	lowerInput := strings.ToLower(userInput)
+	if script, exists := scriptMap[lowerInput]; exists {
+		return string(script)
+	}
+	return userInput
+}
