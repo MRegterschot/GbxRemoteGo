64939270f7eaf0a695bae50d8ff623ad527ad956
MRegterschot <marijn.regterschot@gmail.com>
2025-02-18 16:29:29 +0100

    Add BanAndBlackList method to GbxClient for banning players with a reason and optional save to blacklist

--- TMBanAndBlackList.go
+++ TMBanAndBlackList.go
@@ -1,7 +1,7 @@
 package main
 
 // Ban the player with the specified login, with a message. Add it to the black list, and optionally save the new list. Only available to Admin.
-func (client *GbxClient) BanAndBlacklist(login string, reason string, save bool) error {
-	_, err := client.Send("BanAndBlacklist", login, reason, save)
+func (client *GbxClient) BanAndBlackList(login string, reason string, save bool) error {
+	_, err := client.Send("BanAndBlackList", login, reason, save)
 	return err
 }
