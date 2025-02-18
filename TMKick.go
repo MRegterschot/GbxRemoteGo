dc702a0ff952574acfaaa1989dd241fd99fdc3e9
MRegterschot <marijn.regterschot@gmail.com>
2025-02-18 19:21:17 +0100

    Implement KickId method in GbxClient and remove deprecated TMKickId.go file

--- TMKick.go
+++ TMKick.go
@@ -5,3 +5,9 @@ func (client *GbxClient) Kick(login string, reason string) error {
 	_, err := client.Call("Kick", login, reason)
 	return err
 }
+
+// Kick the player with the specified PlayerId, with an optional message. Only available to Admin.
+func (client *GbxClient) KickId(id int, reason string) error {
+	_, err := client.Call("KickId", id, reason)
+	return err
+}
--- TMKickId.go
+++
@@ -1,7 +0,0 @@
-package main
-
-// Kick the player with the specified PlayerId, with an optional message. Only available to Admin.
-func (client *GbxClient) KickId(id int, reason string) error {
-	_, err := client.Call("KickId", id, reason)
-	return err
-}
